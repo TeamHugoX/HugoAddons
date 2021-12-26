39be772eec1ea4457966953a3617ada716ffed82
OMG <93146537+OMGHelo@users.noreply.github.com>
2021-12-27 00:07:42 +0700

    Create adzan.py

---
+++ adzan.py
@@ -0,0 +1,36 @@
+# Hugo - Userbot
+# 2021 copyright (C)
+
+"""
+**༄** Commands Available -
+• `{i}adzan (city)`
+   Seeing the time for the call to prayer, prayer.
+"""
+
+import requests
+from . import *
+
+
+@hugo_cmd(pattern="adzan(?:\s|$)([\s\S]*)")
+async def get_adzan(adzan):
+    "Shows you the Islamic prayer times of the given city name"
+    input_str = adzan.pattern_match.group(1)
+    LOKASI = gvarstatus("WEATHER_DEFCITY") or "Jakarta" if not input_str else input_str
+    url = f"http://muslimsalat.com/{LOKASI}.json?key=bd099c5825cbedb9aa934e255a81a5fc"
+    request = requests.get(url)
+    if request.status_code != 200:
+        return await edit_delete(
+            adzan, f"**Can't Find City** `{LOCATION}`", 120
+        )
+    result = json.loads(request.text)
+    catresult = f"<b>Jadwal Shalat Hari Ini:</b>\
+            \n<b>📆 Date </b><code>{result['items'][0]['date_for']}</code>\
+            \n<b>📍 City</b> <code>{result['query']}</code> | <code>{result['country']}</code>\
+            \n\n<b>Shurooq  : </b><code>{result['items'][0]['shurooq']}</code>\
+            \n<b>Fajr : </b><code>{result['items'][0]['fajr']}</code>\
+            \n<b>Dhuhr  : </b><code>{result['items'][0]['dhuhr']}</code>\
+            \n<b>Asr  : </b><code>{result['items'][0]['asr']}</code>\
+            \n<b>Maghrib : </b><code>{result['items'][0]['maghrib']}</code>\
+            \n<b>Isha : </b><code>{result['items'][0]['isha']}</code>\
+    "
+    await eor(adzan, catresult, "html")
