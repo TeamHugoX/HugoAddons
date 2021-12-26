0c0324bb1fc7b491255d54d9dd0cbc145851829e
OMG <93146537+OMGHelo@users.noreply.github.com>
2021-12-26 23:39:35 +0700

    Update adzan.py

--- adzan.py
+++ adzan.py
@@ -22,13 +22,17 @@ async def get_adzan(hg):
             hg, f"**Can't Find City** `{LOCATION}`", 120
         )
     result = json.loads(request.text)
-    catresult = f"**Prayer Times Today:**"
-                f"**📆 Date:** `{result['items'][0]['date_for']}`"
-                f"**📍 City:*** `{result['query']}` | `{result['country']}`"
-                f"**Shurooq:** `{result['items'][0]['shurooq']}`"
-                f"**Fajr:** `{result['items'][0]['fajr']}`"
-                f"**Dhur:** `{result['items'][0]['dhuhr']}`"
-                f"**Asr:** `{result['items'][0]['asr']}`"
-                f"**Maghrib:** `{result['items'][0]['maghrib']}`"
-                f"**Isha:** `{result['items'][0]['isha']}`"
+    catresult = f"""
+**Prayer Times Today:**
+
+**📆 Date:** `{result['items'][0]['date_for']}`
+**📍 City:*** `{result['query']}` | `{result['country']}`
+
+**Shurooq:** `{result['items'][0]['shurooq']}`
+**Fajr:** `{result['items'][0]['fajr']}`
+**Dhur:** `{result['items'][0]['dhuhr']}`
+**Asr:** `{result['items'][0]['asr']}`
+**Maghrib:** `{result['items'][0]['maghrib']}`
+**Isha:** `{result['items'][0]['isha']}`
+"""
     await eor(hg, catresult)
